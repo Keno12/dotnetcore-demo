3cccacb03634e83a66a24be3c2b2406e7164bd2e
Keno12 <1598136216@qq.com>
2020-01-14 18:39:07 +0800

    customer respository

---
+++ dotnetcore-demo/dotnetcore-model/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dotnetcore_model
+{
+	public class Customer
+	{
+		public int CustomerID { get; set; }
+
+		public string CustomerFirstName { get; set; }
+
+		public string CustomerLastName { get; set; }
+
+		public bool IsActive { get; set; }
+	}
+}
---
+++ dotnetcore-demo/dotnetcore-respository/ICustomerRespository.cs
@@ -0,0 +1,19 @@
+﻿using dotnetcore_model;
+using System;
+using System.Collections.Generic;
+
+namespace dotnetcore_respository
+{
+    public interface ICustomerRespository
+    {
+        List<Customer> GetCustomers(int amount, string sort);
+
+        Customer GetSingleCustomer(int customerId);
+
+        bool InsertCustomer(Customer ourCustomer);
+
+        bool DeleteCustomer(int customerId);
+
+        bool UpdateCustomer(Customer ourCustomer);
+    }
+}
